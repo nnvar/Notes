b1e29de1cd35e11bcbc2a3e299d44a3a13cc1020
nnvar <ubbercube4@gmail.com>
2024-10-08 17:55:04 -0500

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import NotesPage from "./pages/NotesPage";
 import NotesProvider from "./context/NoteContext";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
  
 function App() {
     return (
+
+        <Router>
+        <Header/>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+        </Routes>
+
         <div id="app">
             <NotesProvider>
                 <NotesPage />
             </NotesProvider>
             
         </div>
+
+    </Router>
+
+        
     );
 }
 
--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -2,11 +2,13 @@ import { useContext } from "react";
 import NoteCard from "../components/NoteCard.jsx";
 import { NoteContext } from "../context/NoteContext.jsx";
 import Controls from "../components/Controls.jsx";
+import Header from "../components/Header.jsx";
  
 const NotesPage = () => {
     const { notes, setNotes } = useContext(NoteContext);
     return (
         <div>
+            <Header></Header>
             {notes.map((note) => (
                 <NoteCard note={note} key={note.$id}  />
             ))}
